ea071ae56c0d01cece2b2e100a516e85864bfa6a
vsd-vector <9928242+vsd-vector@users.noreply.github.com>
2020-12-05 20:49:42 +0200

    DB config example

---
+++ db.example.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "localhost";
+$username = "perehvat";
+$password = "password";
+$db = "perehvat";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$db;charset=utf8mb4", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+
+
+?>
